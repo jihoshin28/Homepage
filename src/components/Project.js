078130e6410008620c4b286dbaac62720c15be41
Allen <jihoshin.28@gmail.com>
2021-06-14 13:59:36 -0700

    added codeitout

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FaNpm } from 'react-icons/fa'
 
 const Project = ({title, description, filter, image, stack, githubLink, liveLink}) => {
     return(
@@ -11,7 +12,13 @@ const Project = ({title, description, filter, image, stack, githubLink, liveLink
                 <p><em>Stack: {stack}</em></p>
                 <div style = {{marginTop: '5%'}} class="portfolio-links">
                   <a href={`${githubLink}`} data-gallery="portfolioGallery" class="portfolio-lightbox" title="Github"><i class="bx bxl-github"></i></a>
-                  <a href={`${liveLink}`} class="portfolio-details-lightbox" data-glightbox="type: external" title="Live Website"><i class="bx bx-window-alt"></i></a>
+                  {
+                    title === "Codeitout"?
+                    <a href={`${liveLink}`} class="portfolio-details-lightbox" data-glightbox="type: external" title="NPM"><FaNpm className = "npm-icon"/></a>
+                    :
+                    <a href={`${liveLink}`} class="portfolio-details-lightbox" data-glightbox="type: external" title="Live Website"><i class="bx bx-window-alt"></i></a>
+                  }
+
                 </div>
                 <div>
                 <ul style= {{marginBottom: '-10%'}}id="portfolio-flters">
--- src/components/sections/Portfolio.js
+++ src/components/sections/Portfolio.js
@@ -16,21 +16,20 @@ const Portfolio = () => {
           <div class="col-lg-12 d-flex justify-content-center" data-aos="fade-up" data-aos-delay="100">
             <ul id="portfolio-flters">
               <li data-filter=".filter-project" class="filter-active">All</li>
+              <li data-filter=".filter-codeitout">Codeitout</li>
               <li data-filter=".filter-breadBasket">BreadBasket</li>
               <li data-filter=".filter-nutritionTracker">NutritionTracker</li>
               <li data-filter=".filter-movieReviewer">Movie Reviewer</li>
-              <li data-filter=".filter-roastToast">RoastToast</li>
             </ul>
           </div>
         </div>
 
         <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="200">
-
+            <Project title = "Codeitout" stack = "React, Node.js, Express" description= "Browser code editing tool" filter = 'filter-codeitout' image = {process.env.PUBLIC_URL + './img/portfolio/codeitout.png'} githubLink = "https://github.com/jihoshin28/codeitout" liveLink = "https://www.npmjs.com/package/codeitout/"/>
             <Project title = "BreadBasket Shopper" stack = "React, Redux, Ruby on Rails" description= "Grocery shopping web application" filter = 'filter-breadBasket' image = {process.env.PUBLIC_URL + './img/portfolio/shopper.png'} githubLink = "https://github.com/jihoshin28/BreadBasket_Shopper" liveLink = "https://www.breadbasketdelivery.com/"/>
             <Project title = "BreadBasket Driver" stack = "React Native, Redux, Ruby on Rails" description = "Grocery delivery mobile application" filter = 'filter-breadBasket' image = {process.env.PUBLIC_URL + './img/portfolio/driver.webp'} githubLink = "https://github.com/jihoshin28/BreadBasket_Driver" liveLink = ""/>
             <Project title = "Nutrition Tracker" stack = "React, Ruby on Rails" description = "Diet and exercise tracking web application" filter = 'filter-nutritionTracker' image = {process.env.PUBLIC_URL + './img/portfolio/nutrition_tracker.png'} githubLink = "https://github.com/jihoshin28/Nutrition_Tracker_2.0_frontend" liveLink = "https://www.nutritiontracker.org/"/>
             <Project title = "Movie Reviewer" stack = "Ruby on Rails" description = "Movie review website" filter = "filter-movieReviewer" image = {process.env.PUBLIC_URL + './img/portfolio/movie_reviewer.png'} githubLink = "https://github.com/jihoshin28/Movie_Reviewer_2019" liveLink = "https://movie-reviewer-rb.herokuapp.com/"/>
-            <Project title = "Roast n Toast" stack = "React, Ruby on Rails" description = "Forum website for roasting and toasting" filter = "filter-roastToast" image = {process.env.PUBLIC_URL + './img/portfolio/roastnToast.png'} githubLink = "https://github.com/jihoshin28/roastntoast-frontend" liveLink = "https://roast-n-toast.herokuapp.com/"/>
         </div>
 
       </div>
